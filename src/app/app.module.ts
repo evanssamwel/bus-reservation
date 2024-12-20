c94862f69f5c5e5ffd45625a7d266c26d8ae75a2
evanssamwel <nyamuevans@gmail.com>
2024-12-20 16:13:19 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    // ... other components
+  ],
+  imports: [
+    // ... other modules
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.username, this.password).subscribe(
+      (response) => {
+        // Handle successful login, e.g., store the token
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        // Handle login error
+        console.error(error);
+      }
+    );
+  }
+}
